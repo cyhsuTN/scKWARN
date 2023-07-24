39d3c69acd814e796e6daee4c54d115f7a3c3c58
Chih-Yuan Hsu <53067662+cyhsuTN@users.noreply.github.com>
2023-07-24 13:10:57 -0500

    Add files via upload

---
+++ R/Core_functions.R
@@ -0,0 +1,75 @@
+
+
+### a function for normalization
+asnfast7 <- function(test_data, bw.method) {
+
+  colnames(test_data) <- NULL
+  rownames(test_data) <- NULL
+  
+  test_data <- test_data[rowSums(test_data>0)>3,]
+
+  ## quantiles for expression profiles of cells
+  sj <- apply(test_data, 2, function(x) quantile(log(x[x>0]), probs = c(.25, .5, .75)) )
+  sj <- sj[apply(sj, 1, sd) != 0, , drop = FALSE]
+  ### To calculate PC1
+  pca <- prcomp(t(sj), scale = TRUE) # n by p matrix for prcomp
+  r.all <- c(pca$x[,1]) ## use the first component
+  #r.all <- c(t(pca$rotation[, 1]) %*% sj) ## use the first component
+  ### list non-zero cells for every gene
+  #nonzero.g <- apply(test_data, 1, function(x) {cc <- which(x>0); rbind(cc, log(x[cc]), deparse.level = 0)})
+  nonzero.g <- apply(test_data, 1, function(x) {cc <- which(x>0); list(rbind(cc, log(x[cc]), deparse.level = 0))})
+  nonzero.g <- lapply(nonzero.g, function(x) matrix(unlist(x), nrow=2))
+  ### reference profile
+  AllAve <- unlist(lapply(nonzero.g, function(x) mean(x[2,])))
+  ### list non-zero genes for every cell
+  #nonzero.c <- apply(test_data, 2, function(x) which(x>0) )
+  nonzero.c <- apply(test_data, 2, function(x) list(which(x>0)) )
+  nonzero.c <- lapply(nonzero.c, function(x) c(unlist(x)))
+  
+  ### remove data which will not be used again
+  rm(test_data, sj, pca); gc()
+
+  if (!is.na(bw.method) & bw.method=="RoT") {
+    hh <- sapply(nonzero.g, function(x) {
+      bw.nrd0(r.all[x[1,]])
+    })
+  } else {
+    hh <- sapply(nonzero.g, function(x) {
+      h <- tryCatch({
+        bw.SJ(r.all[x[1,]], method="ste") # method = "ste" or "dpi"
+      }, error = function(e) {
+        return(bw.nrd0(r.all[x[1,]]))
+      })
+    })
+  }
+  
+  ### expression profile for each cell
+  LocAve <- calculateLocAve(nonzero.c, nonzero.g, r.all, hh)
+  
+  ### global scaling factor for each cell
+  logscalingF <- rep(0, length(LocAve))
+  for (j in 1:length(LocAve)) {
+    logscalingF[j] <- median(LocAve[[j]] - AllAve[nonzero.c[[j]]], na.rm=T) - 1E-10
+  }
+
+  exp(logscalingF - median(logscalingF))
+
+}
+
+
+### a function for rescale
+scale_matrix7 <- function(mg, cgs, qc_conditions, qc_countmatrix) {
+  
+  indxall <- qc_countmatrix != 0
+  qc_countmatrix@x <- log(qc_countmatrix@x)
+  refall <- rowSums(qc_countmatrix)/rowSums(indxall)
+  
+  remg <- rep(NA, ncol(qc_countmatrix))
+  for(k in 1:length(cgs)) {
+    ck <- which(qc_conditions == cgs[k])
+    remg[ck] <- mg[ck] * 
+      exp(median(rowSums(qc_countmatrix[, ck, drop = FALSE])/rowSums(indxall[, ck, drop = FALSE]) - refall))
+  }
+  remg
+  
+}
---
+++ R/LocASN.R
@@ -0,0 +1,124 @@
+#' Single-cell RNA sequencing normalization using a local average technique
+#' @description A function of normalizing single cell RNA-seq gene expression.
+#' @import Matrix
+#' @import Rcpp
+#' @importFrom methods as is
+#' @importFrom stats density median prcomp quantile sd bw.nrd0 bw.SJ
+
+#' @param countmatrix Input. Unnormalized count matrix (genes by cells).
+#' @param conditions  Input (Optional). Indicate which cells are sampled from the same conditions.
+#' The default value, NULL, denotes all the cells are sampled from the same condition.
+#' @param filter  Input (Optional). A logic value to indicate if need data filtering. If TRUE, please see
+#' the details of gene_num_gezero and cell_num_gezero for input. The default value is FALSE.
+#' @param gene_num_gezero Input (Optional). A threshold (interger) to determine the inclusion of a gene.
+#' The gene included needs to be expressed in at least \emph{gene_num_gezero} cells.
+#' The default value is 3.
+#' @param cell_num_gezero Input (Optional). A threshold (interger) to determine the inclusion of a cell.
+#' The cell included needs to contain at least \emph{cell_num_gezero} expressed genes.
+#' The default value is 10.
+#' @param numGeneforEst Input (Optional). Use top \emph{numGeneforEst} (integer) genes according to 
+#' the proportions of gene counts > 0 in cells to estimate the scaling factors, 
+#' for speeding up computation.
+#' @param divideforFast Input (Optional). A logic value to indicate if speeding up computation
+#' by randomly dividing cells in each condition into \emph{numDivide} smaller groups.
+#' Please input an integer in \emph{numDivide} below if \emph{divideforFast} = TRUE.
+#' The default value is TRUE.
+#' @param numDivide Input (Optional). An integer is required if \emph{divideforFast} = TRUE.
+#' The cells in each condition will be randomly divided by \emph{numDivide} small groups.
+#' The default \emph{numDivide} = NULL will automatically use the maximum of 1 and the smallest integer 
+#' that is not less than the number of cells in each condition divided by 3K, 
+#' that means no division for conditions with less than 3K cells.
+#' @param bw.method Input (Optional). A method to estimate the bandwidths in Kernel weighting. 
+#' The default method uses "SJ" (SJ bandwidth, Sheather and Jones, 1991). 
+#' Otherwise, uses "RoT" (rule-of-thumb, Silverman, 1986).
+
+
+#' @return \item{NormalizedData}{Matrix (genes by cells). Data matrix after normalization.}
+#' @return \item{scalingFactor}{Vector. Cell-specific scaling factors.}
+#' @return \item{delete_genes}{Vector. Indeice of the genes deleted.}
+#' @return \item{delete_cells}{Vector. Indeice of the cells deleted.}
+
+#' @examples set.seed(12345)
+#' @examples G <- 2000; n <- 600 # G: number of genes, n: number of cells
+#' @examples NB_cell <- function(j) rnbinom(G, size = 0.1, mu = rgamma(G, shape = 2, rate = 2))
+#' @examples countsimdata <- sapply(1:n, NB_cell)
+#' @examples colnames(countsimdata) <- paste("c", 1:n, sep = "_")
+#' @examples rownames(countsimdata) <- paste("g", 1:G, sep = "_")
+#' @examples Result <- LocASN(countmatrix = as(countsimdata,"sparseMatrix"))
+#' @examples Result$NormalizedData[1:10,1:10]; Result$scalingFactor[1:10]
+#' @examples
+#'
+
+#' @examples #conditions <- c(rep(1,n/2), rep(2,n/2))
+#' @examples #Result2 <- LocASN(countmatrix = countsimdata, conditions = conditions)
+#' @examples #Result2$NormalizedData[1:10,1:10]; Result2$scalingFactor[1:10]
+
+
+#' @export
+LocASN <- function(countmatrix, conditions = NULL, filter = FALSE, 
+                   gene_num_gezero = 3, cell_num_gezero = 10, 
+                   numGeneforEst = 2000, divideforFast = TRUE, numDivide = NULL, bw.method = "SJ") {
+
+  ### determine if sparse matrix. Transform dense matrix to sparse matrix if not.
+  if (!is(countmatrix, "sparseMatrix")) countmatrix <- as(countmatrix, "sparseMatrix")
+  
+  ### whether to randomly divide cells in each condition into \emph{numDivide} groups
+  if (divideforFast) {
+    set.seed(2020)
+    if (is.null(conditions)) {
+      if (is.null(numDivide)) autonum <- max(1, ceiling(ncol(countmatrix)/3000)) else autonum <- numDivide
+      conditions <- sample(c(1:autonum), ncol(countmatrix), replace = T)
+    } else {
+      cgs <- unique(conditions)
+      for (k in 1:length(cgs)) {
+        ck <- which(conditions == cgs[k])
+        if (is.null(numDivide)) autonum <- max(1, ceiling(length(ck)/3000)) else autonum <- numDivide
+        conditions[ck] <- paste0(conditions[ck], "_", sample(c(1:autonum), length(ck), replace = T))
+      }
+    }  
+  }
+
+  if (filter) {
+    conditions <- if (is.null(conditions)) rep(1, ncol(countmatrix)) else conditions
+    ### simple data filtering
+    delete_genes <- which(Matrix::rowSums(countmatrix > 0) < gene_num_gezero)
+    delete_cells <- which(Matrix::colSums(countmatrix > 0) < cell_num_gezero)
+    if (length(delete_genes)>0 & length(delete_cells)>0) {
+      countmatrix <- countmatrix[-c(delete_genes), -c(delete_cells)]
+      conditions <- conditions[-c(delete_cells)]
+    } else if (length(delete_genes)==0 & length(delete_cells)>0) {
+      countmatrix <- countmatrix[, -c(delete_cells)]
+      conditions <- conditions[-c(delete_cells)]
+    } else if (length(delete_genes)>0 & length(delete_cells)==0) {
+      countmatrix <- countmatrix[-c(delete_genes), ]
+    }
+  } else {
+    delete_genes <- delete_cells <- which(NA)
+  }
+ 
+  ### Determine how many genes are used for estimating scaling factors 
+  #if(is.null(numGeneforEst)) numGeneforEst <- sum(rowMeans(countmatrix>0) > 0.3)
+  genesuse <- head(order(rowSums(countmatrix > 0), decreasing = T), numGeneforEst)
+  
+  if (is.null(conditions) | length(unique(conditions))==1) {
+    remg <- asnfast7(countmatrix[genesuse,], bw.method)
+    countmatrix <- t(t(countmatrix)/remg)
+  } else {
+    cgs <- unique(conditions)
+    mg <- rep(NA, ncol(countmatrix))
+    for(k in 1:length(cgs)) {
+      ck <- which(conditions == cgs[k])
+      mg[ck] <- asnfast7(countmatrix[genesuse, ck, drop = FALSE], bw.method)
+      countmatrix[, ck] <- t(t(countmatrix[, ck, drop = FALSE])/mg[ck])
+    }
+    ### rescale multiple normailzed matrices
+    remg <- scale_matrix7(mg, cgs, conditions, countmatrix[genesuse,])
+    remg <- remg/median(remg)
+    countmatrix  <- t(t(countmatrix) * mg / remg)
+  }
+
+  list(NormalizedData = countmatrix, scalingFactor = remg,
+       delete_genes = delete_genes, delete_cells = delete_cells)
+}
+
+
